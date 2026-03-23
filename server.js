46b4fe230d793b7f316a250216663c4dc22e356a
Rockson <rockson.omane94@gmail.com>
2026-03-23 11:46:53 +0100

    Updated UI and Gemini model used

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
+import express from 'express';
+const app = express ();
 const PORT = 8000
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-
+import { GoogleGenAI} from "@google/genai";
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
 app.use(express.json())
 app.use(cors())
 
-const {GoogleGenerativeAI} = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI(process.env.LANGUAGE_MODEL_API_KEY)
 
-
-
-app.get('/prompt/:text', async(req, res) => {
-    const model = genAI.getGenerativeModel({model: "gemini-pro"})
-    const text = req.params.text
-    const chat = model.startChat({
-        history:[{
-            role: "user",
-            parts: [{ text: "Hello" }],
-        },
-        {   role: "model",
-            parts: [{ text: "Hi" }],
-        }
-    ]
-    })
-    const result = await chat.sendMessage(text)
-    const response = result.response;
-    const data = response.text()
-    console.log(data);
-    res.send(data)
+const apiKey = process.env.apiKey;
+const ai = new GoogleGenAI({apiKey});
+app.get('/prompt/:text', async function main(req, res) {
+  const text = req.params.text
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: text,
+  }); 
+  const data = response.text
+  console.log(data);
+  res.send(data)
+  console.log(response.contents);
 })
+
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
